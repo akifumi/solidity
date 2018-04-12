d435772099afde122d3f48556ba7f08e212e19ae
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2018-04-12 23:19:11 +0900

    Created DeleteExample.sol

---
+++ contracts/DeleteExample.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.0;
+
+
+contract DeleteExample {
+    uint data;
+    uint[] dataArray;
+
+    function f() {
+        uint x = data;
+        // xが0で初期化される
+        delete x;
+        // dataが0で初期化される
+        // xはdataの値型をcopyしているため影響はありません
+        delete data;
+        uint[] y = dataArray;
+        // dataArray.lengthを0にする
+        // yはdataArrayへの参照のため、y.lengthも0になります
+        // 参照から実体をdeleteできないため、delete yはエラーになります
+        delete dataArray;
+    }
+}
